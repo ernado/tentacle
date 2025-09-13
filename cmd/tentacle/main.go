cadc92686878a0e76b53e914df92f3309f99d55d
Aleksandr Razumov <ernado@go-faster.org>
2025-09-13 15:02:13 +0300

    feat: add bot

--- cmd/tentacle/main.go
+++ cmd/tentacle/main.go
@@ -1,15 +1,200 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"os/exec"
+	"time"
 
+	"github.com/ernado/tentacle/internal/ytdlp"
+	"github.com/go-faster/errors"
 	"github.com/go-faster/sdk/app"
+	"github.com/gotd/td/telegram"
+	"github.com/gotd/td/telegram/message"
+	"github.com/gotd/td/tg"
 	"go.uber.org/zap"
 )
 
+func BestVideo(formats []ytdlp.Format) ytdlp.Format {
+	var best ytdlp.Format
+	for _, f := range formats {
+		if f.VCodec == "none" {
+			continue
+		}
+		best = f
+	}
+
+	return best
+}
+
+func BestAudio(formats []ytdlp.Format) ytdlp.Format {
+	var best ytdlp.Format
+	for _, f := range formats {
+		if f.ACodec == "none" {
+			continue
+		}
+		best = f
+	}
+
+	return best
+}
+
 func main() {
 	app.Run(func(ctx context.Context, lg *zap.Logger, t *app.Telemetry) error {
-		<-t.ShutdownContext().Done()
+		dispatcher := tg.NewUpdateDispatcher()
+		opt := telegram.Options{
+			UpdateHandler: dispatcher,
+			Logger:        lg.Named("gotd"),
+		}
+
+		if err := telegram.BotFromEnvironment(ctx, opt, func(ctx context.Context, client *telegram.Client) error {
+			var (
+				api    = tg.NewClient(client)
+				sender = message.NewSender(api)
+			)
+			dispatcher.OnNewMessage(func(ctx context.Context, e tg.Entities, u *tg.UpdateNewMessage) error {
+				ctx, cancel := context.WithTimeout(ctx, time.Minute*30)
+				defer cancel()
+
+				m, ok := u.Message.(*tg.Message)
+				if !ok || m.Out {
+					return nil
+				}
+
+				lg := lg.With(zap.Int("msg_id", m.ID))
+
+				uri, err := url.Parse(m.Message)
+				if err != nil {
+					return errors.Wrap(err, "parse url")
+				}
+
+				buf := new(bytes.Buffer)
+				stderr := new(bytes.Buffer)
+				{
+					additionalArgs := os.Args[1:]
+
+					// 1. Get JSON video info.
+					args := []string{
+						"-j",
+						"--cookies-from-browser", "firefox",
+						uri.String(),
+					}
+					args = append(args, additionalArgs...)
+					cmd := exec.CommandContext(ctx, "yt-dlp", args...)
+
+					cmd.Stdout = buf
+					cmd.Stderr = stderr
+					if err := cmd.Run(); err != nil {
+						return errors.Wrapf(err, "yt-dlp: %s", stderr.String())
+					}
+				}
+
+				var video ytdlp.Video
+				if err := json.Unmarshal(buf.Bytes(), &video); err != nil {
+					return errors.Wrap(err, "unmarshal video info")
+				}
+
+				bestVideo := BestVideo(video.Formats)
+				bestAudio := BestAudio(video.Formats)
+
+				mux := http.NewServeMux()
+				copyRequest := func(f ytdlp.Format) http.Handler {
+					return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						req, err := http.NewRequestWithContext(r.Context(), r.Method, f.URL, nil)
+						if err != nil {
+							http.Error(w, err.Error(), http.StatusInternalServerError)
+							return
+						}
+						for k := range r.Header {
+							req.Header.Set(k, r.Header.Get(k))
+						}
+						for k, v := range bestVideo.HTTPHeaders {
+							req.Header.Set(k, v)
+						}
+						res, err := http.DefaultClient.Do(req)
+						if err != nil {
+							http.Error(w, err.Error(), http.StatusInternalServerError)
+							return
+						}
+						defer func() {
+							_ = res.Body.Close()
+						}()
+						for k := range res.Header {
+							w.Header().Set(k, res.Header.Get(k))
+						}
+						w.WriteHeader(res.StatusCode)
+						_, _ = io.Copy(w, res.Body)
+					})
+				}
+				mux.Handle("/video.mp4", copyRequest(bestVideo))
+				mux.Handle("/audio.m4a", copyRequest(bestAudio))
+
+				ln, err := net.Listen("tcp", "localhost:0")
+				if err != nil {
+					return errors.Wrap(err, "listen")
+				}
+
+				srv := &http.Server{
+					Handler: mux,
+				}
+
+				go func() {
+					<-ctx.Done()
+					shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), time.Second*5)
+					defer shutdownCancel()
+					if err := srv.Shutdown(shutdownCtx); err != nil {
+						lg.Error("shutdown", zap.Error(err))
+					}
+				}()
+				go func() {
+					if err := srv.Serve(ln); err != nil && !errors.Is(err, http.ErrServerClosed) {
+						lg.Error("serve", zap.Error(err))
+					}
+				}()
+
+				outputFileName := fmt.Sprintf("%d.mp4", m.ID)
+				inputHTTPAddress := fmt.Sprintf("http://%s", ln.Addr().String())
+
+				{
+
+					cmd := exec.CommandContext(ctx, "ffmpeg",
+						"-i", inputHTTPAddress+"/video.mp4",
+						"-i", inputHTTPAddress+"/audio.m4a",
+						"-c:v", "copy",
+						"-c:a", "copy",
+						"-f", "mp4",
+						"-y",
+						outputFileName,
+					)
+					cmd.Env = []string{}
+					cmd.Stdout = os.Stdout
+					cmd.Stderr = os.Stderr
+
+					if err := cmd.Run(); err != nil {
+						return errors.Wrapf(err, "ffmpeg: %s", stderr.String())
+					}
+				}
+
+				answer := sender.Answer(e, u)
+
+				if _, err := answer.Textf(ctx, "Uploading..."); err != nil {
+					return errors.Wrap(err, "send answer")
+				}
+
+				return nil
+			})
+
+			return nil
+		}, telegram.RunUntilCanceled); err != nil {
+			panic(err)
+		}
 		return nil
 	})
 }
--- go.mod
+++ go.mod
@@ -3,28 +3,44 @@ module github.com/ernado/tentacle
 go 1.24.0
 
 require (
+	github.com/ernado/ff v0.2.0
 	github.com/go-faster/errors v0.7.1
 	github.com/go-faster/sdk v0.28.0
+	github.com/gotd/td v0.131.0
 	github.com/stretchr/testify v1.11.1
 	go.uber.org/zap v1.27.0
+	golang.org/x/sync v0.16.0
 )
 
 require (
 	github.com/KimMachineGun/automemlimit v0.7.4 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cenkalti/backoff/v5 v5.0.2 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/coder/websocket v1.8.13 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dlclark/regexp2 v1.11.5 // indirect
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-faster/jx v1.1.0 // indirect
+	github.com/go-faster/xor v1.0.0 // indirect
+	github.com/go-faster/yaml v0.4.6 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-logr/zapr v1.3.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/gotd/ige v0.2.2 // indirect
+	github.com/gotd/neo v0.1.5 // indirect
 	github.com/grafana/otel-profiling-go v0.5.1 // indirect
 	github.com/grafana/pyroscope-go v1.2.4 // indirect
 	github.com/grafana/pyroscope-go/godeltaprof v0.1.8 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.27.1 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/ogen-go/ogen v1.14.0 // indirect
 	github.com/pbnjay/memory v0.0.0-20210728143218-7b4eea64cf58 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_golang v1.22.0 // indirect
@@ -34,6 +50,7 @@ require (
 	github.com/samber/lo v1.47.0 // indirect
 	github.com/samber/slog-common v0.18.1 // indirect
 	github.com/samber/slog-zap/v2 v2.6.2 // indirect
+	github.com/segmentio/asm v1.2.0 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/contrib/bridges/otelzap v0.12.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/runtime v0.62.0 // indirect
@@ -61,15 +78,21 @@ require (
 	go.opentelemetry.io/otel/sdk/metric v1.37.0 // indirect
 	go.opentelemetry.io/otel/trace v1.37.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.7.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/automaxprocs v1.6.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/net v0.41.0 // indirect
-	golang.org/x/sync v0.16.0 // indirect
-	golang.org/x/sys v0.33.0 // indirect
-	golang.org/x/text v0.26.0 // indirect
+	golang.org/x/crypto v0.41.0 // indirect
+	golang.org/x/exp v0.0.0-20230725093048-515e97ebf090 // indirect
+	golang.org/x/mod v0.27.0 // indirect
+	golang.org/x/net v0.43.0 // indirect
+	golang.org/x/sys v0.35.0 // indirect
+	golang.org/x/text v0.28.0 // indirect
+	golang.org/x/tools v0.36.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20250603155806-513f23925822 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250603155806-513f23925822 // indirect
 	google.golang.org/grpc v1.74.0 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	rsc.io/qr v0.2.0 // indirect
 )
